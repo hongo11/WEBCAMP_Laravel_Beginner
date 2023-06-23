747c1599b130961ad3df597ab7eb1c341349ff4a
ryo <hanauta0226@gmail.com>
2023-06-23 14:39:39 +0000

    会員登録(簡易)を追加

--- resources/views/user/register.blade.php
+++ resources/views/user/register.blade.php
@@ -1,31 +1,34 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>会員登録</title>
+    <title>ユーザ登録</title>
 </head>
 <body>
-    <h1>会員登録</h1>
+    <h1>ユーザ登録</h1>
 
-    @if ($errors->any())
-        <ul>
-            @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
-            @endforeach
-        </ul>
-    @endif
+   @if ($errors->any())
+    <div>
+    @foreach ($errors->all() as $error)
+        {{ $error }}<br>
+    @endforeach
+</div>
+
+@endif
+
 
     <form method="POST" action="{{ route('user.register.post') }}">
         @csrf
 
         <label for="name">名前:</label>
-        <input type="text" id="name" name="name" required maxlength="128"><br><br>
+        <input type="text" id="name" name="name" value="{{ old('name') }}" maxlength="128"><br>
 
-        <label for="email">メールアドレス:</label>
-        <input type="email" id="email" name="email" required maxlength="254"><br><br>
+        <label for="email">email:</label>
+        <input type="email" id="email" name="email" value="{{ old('email') }}" maxlength="254"><br>
 
         <label for="password">パスワード:</label>
-        <input type="password" id="password" name="password" required maxlength="72"><br><br>
+        <input type="password" id="password" name="password" maxlength="72"><br>
 
-        <input type="submit" value="登録">
+        <input type="submit" value="登録する">
     </form>
-</body
+</body>
+</html>
